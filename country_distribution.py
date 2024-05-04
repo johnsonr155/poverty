7a4f09c16b2cb24f2ea7c4216c60b40935f8cfc2
Robert Johnson <robertajohnson401@gmail.com>
2024-05-04 14:15:24 +0100

    adding distributuion plot

---
+++ country_distribution.py
@@ -0,0 +1,80 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.ticker import FuncFormatter
+import matplotlib as mpl
+
+from process_data import rename_raw_data
+
+
+# select columns we're interested in for looking at disrribution
+def simplify_data(df):
+    #rename cols
+    df = df[["country", "date", "Poverty headcount ratio at $2.15 a day (2017 PPP) (% of population)", "GDP per capita (current US$)", "Population, total"]]
+    shortened_metric = {
+        "Poverty headcount ratio at $2.15 a day (2017 PPP) (% of population)":"poverty",
+        "GDP per capita (current US$)": "GDP per capita",
+        "Population, total": "population"
+    }
+    df.rename(columns=shortened_metric, inplace=True)
+    return(df)
+
+# llooking only at the most recent data for each country 
+def max_recent_yr(df, min_yr = 2010):
+    df = df[df['poverty'].notnull()]
+    df = df[df["date"] >= min_yr]
+    df = df.groupby('country').apply(lambda x: x.loc[x['date'].idxmax()])
+    df.reset_index(drop=True, inplace=True)
+    return(df)
+
+
+df_renamed = rename_raw_data()
+df_recent_yr = simplify_data(df_renamed)
+max_year_poverty_by_country = max_recent_yr(df_recent_yr)
+
+
+# Plot bubble chart
+# Customize axis tick labels 
+def percent_formatter(x, pos):
+    return f"{x:.0f}%"
+
+def dollar_thousand_formatter(x, pos):
+    return '${:,.0f}'.format(x)
+
+mpl.rcParams.update({'font.size': 14})
+
+plt.figure(figsize=(10, 6))
+
+plt.scatter(max_year_poverty_by_country['GDP per capita'], max_year_poverty_by_country['poverty'], s=max_year_poverty_by_country['population'] / 1e6, alpha=0.5)
+plt.xscale('log')
+plt.gca().xaxis.set_major_formatter(ticker.ScalarFormatter())
+plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(dollar_thousand_formatter))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(percent_formatter))
+plt.axvspan(750, 2000, color='gray', alpha=0.3)
+plt.text(1500, plt.ylim()[1]*0.9, 'Corridor of Variation', fontsize=10, ha='center')
+# Set labels and title
+plt.xlabel('GDP per capita')
+plt.ylabel('Percentage in poverty')
+plt.title('Poverty vs. GDP per Capita')
+# Show plot
+plt.grid(True)
+plt.show()
+
+# focus plot
+
+plt.figure(figsize=(10, 6))
+plt.scatter(max_year_poverty_by_country['GDP per capita'], max_year_poverty_by_country['poverty'], s=max_year_poverty_by_country['population'] / 1e5, alpha=0.5)
+plt.gca().xaxis.set_major_formatter(ticker.ScalarFormatter())
+plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(dollar_thousand_formatter))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(percent_formatter))
+plt.xlim(750, 2000)
+plt.ylim(0, 70)
+# Set labels and title
+plt.xlabel('GDP per capita')
+plt.ylabel('Percentage in poverty')
+plt.title('Corridor of Variation: Poverty vs. GDP per Capita')
+for i, row in max_year_poverty_by_country.iterrows():
+    plt.annotate(row['country'], (row['GDP per capita'], row['poverty']), textcoords="offset points", xytext=(5,5), ha='left', fontsize=11)
+# Show plot
+plt.grid(True)
+plt.show()
---
+++ process_data.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+EXLCUDED_REGIONS = [
+    "Africa Western and Central",
+    "Africa Eastern and Southern",
+    "Arab World",
+    "Central Europe and the Baltics",
+    "East Asia & Pacific (excluding high income)",
+    "Early-demographic dividend",
+    "East Asia & Pacific",
+    "Europe & Central Asia (excluding high income)",
+    "Europe & Central Asia",
+    "Euro area",
+    "European Union",
+    "Fragile and conflict affected situations",
+    "Heavily indebted poor countries (HIPC)",
+    "IBRD only",
+    "IDA & IBRD total",
+    "IDA total",
+    "IDA blend",
+    "Latin America & Caribbean (excluding high income)",
+    "Least developed countries: UN classification",
+    "Low & middle income",
+    "Late-demographic dividend",
+    "Middle East & North Africa (excluding high income)",
+    "Middle income",
+    "OECD members",
+    "Other small states",
+    "Pre-demographic dividend",
+    "Pacific island small states",
+    "Post-demographic dividend",
+    "Sub-Saharan Africa (excluding high income)",
+    "Sub-Saharan Africa",
+    "Small states",
+    "East Asia & Pacific (IDA & IBRD countries)",
+    "Europe & Central Asia (IDA & IBRD countries)",
+    "Latin America & the Caribbean (IDA & IBRD countries)",
+    "Middle East & North Africa (IDA & IBRD countries)",
+    "South Asia (IDA & IBRD)",
+    "Sub-Saharan Africa (IDA & IBRD countries)",
+    "World",
+    "High income",
+    "Low income",
+    "Lower middle income"
+    "Upper middle income",
+    "Not classified"] 
+
+def rename_raw_data(raw_data_filepath = "data/data_manager.csv", indicator_ids_filepath = "data/indicator_ids.csv"):
+    #read in data
+    df_raw = pd.read_csv(raw_data_filepath)
+    df_indicator = pd.read_csv(indicator_ids_filepath)
+    df_raw = df_raw[~df_raw['country'].isin(EXLCUDED_REGIONS)]
+    #rename columns
+    indicator_codes = set(df_raw.columns)
+    df_indicator_filtered = df_indicator[df_indicator['indicator_id'].isin(indicator_codes)]
+    code_to_metric = dict(zip(df_indicator_filtered['indicator_id'], df_indicator_filtered['indicator']))
+    df_raw.rename(columns=code_to_metric, inplace=True)
+    return(df_raw)
